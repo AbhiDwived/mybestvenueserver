d2a8cc78b68efb69771c057530219ab7d5b575d6
Abhinandan <abhinandan@mybestvenue.com>
2025-08-29 15:12:32 +0530

    cors fix

--- server.js
+++ server.js
@@ -209,8 +209,9 @@ app.use(
   cors({
     origin: function (origin, callback) {
       const allowedOrigins = [
-        'http://localhost:5173',
-        'http://localhost:5174',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://vendors.mybestvenue.com',
         'https://mybestvenue.com',
         'https://www.mybestvenue.com'
       ];
